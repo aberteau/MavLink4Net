75dd879eb55e57f6e7b2007986741a68cf642024
aberteau <amael.berteau@tech-easy.fr>
2018-02-28 00:45:22 +0100

    Code generation improvements (MavType et Enum)

--- CodeGenerator.Core/GeneratorHelper.cs
+++ CodeGenerator.Core/GeneratorHelper.cs
@@ -1,19 +1,42 @@
 ﻿using System;
 using System.CodeDom;
 using System.CodeDom.Compiler;
+using System.Collections.Generic;
 using System.IO;
+using MavLink4Net.MessageDefinitions.Data;
 using Microsoft.CSharp;
 
 namespace MavLink4Net.CodeGenerator.Core
 {
     public class GeneratorHelper
     {
-        public static void Generate(string language, string outputPath)
+        private const string MessageBaseClassName = "Message";
+        private const string CommonName = "Common";
+        private const string MavTypeEnumName = "MavType";
+
+        public static void Generate(MavLink mavLink, string language, string outputPath, string ns)
         {
             CodeGeneratorOptions options = new CodeGeneratorOptions() { BracingStyle = "C" };
             CodeDomProvider codeProvider = CreateCodeDomProvider(language);
-            
-            GenerateMessageClassFile(outputPath, "Heartbeat", options, codeProvider);
+
+            GenerateMavTypeEnum(outputPath, mavLink.Messages, options, codeProvider, ns, MavTypeEnumName);
+
+            string commonNamespace = $"{ns}.{CommonName}";
+            string commonPath = Path.Combine(outputPath, CommonName);
+
+            GenerateEnumFiles(commonPath, mavLink.Enums, options, codeProvider, commonNamespace);
+
+            string baseClassFullName = $"{ns}.{MessageBaseClassName}";
+            string messageTypeEnumFullName = $"{ns}.{MavTypeEnumName}";
+            GenerateMessageClassFiles(commonPath, mavLink.Messages, options, codeProvider, commonNamespace, baseClassFullName, messageTypeEnumFullName);
+        }
+
+        private static void GenerateMavTypeEnum(string outputPath, IEnumerable<Message> messages, CodeGeneratorOptions options, CodeDomProvider codeProvider, string ns, string messageTypeEnumFullName)
+        {
+            string filename = $"{messageTypeEnumFullName}.cs";
+            String filePath = Path.Combine(outputPath, filename);
+            CodeCompileUnit unit = MavTypeEnumGeneratorHelper.CreateCodeCompileUnit(messageTypeEnumFullName, messages, ns);
+            codeProvider.GenerateCodeFromCompileUnit(unit, options, filePath);
         }
 
         private static CodeDomProvider CreateCodeDomProvider(string language)
@@ -22,14 +45,46 @@ namespace MavLink4Net.CodeGenerator.Core
             return new CSharpCodeProvider();
         }
 
-        private static void GenerateMessageClassFile(string folderPath, String messageClassName, CodeGeneratorOptions options, CodeDomProvider codeProvider)
+        private static void GenerateMessageClassFiles(string folderPath, IEnumerable<MessageDefinitions.Data.Message> messages, CodeGeneratorOptions options, CodeDomProvider codeProvider, string ns, string baseClassName, string messageTypeEnumFullName)
         {
-            string filename = $"{messageClassName}.cs";
+            foreach (MessageDefinitions.Data.Message message in messages)
+            {
+                string messageTypeEnumValue = $"{messageTypeEnumFullName}.{message.Name}";
+                GenerateMessageClassFile(folderPath, message, options, codeProvider, ns, baseClassName, messageTypeEnumValue);
+            }
+        }
+
+        private static void GenerateMessageClassFile(string folderPath, MessageDefinitions.Data.Message message, CodeGeneratorOptions options, CodeDomProvider codeProvider, string ns, string baseClassName, string messageTypeEnumValue)
+        {
+            string messageName = GetMessageClassName(message);
+            string filename = $"{messageName}.cs";
             String filePath = Path.Combine(folderPath, filename);
-            CodeCompileUnit unit = MessageGeneratorHelper.CreateCodeCompileUnit(messageClassName);
+            CodeCompileUnit unit = MessageGeneratorHelper.CreateCodeCompileUnit(message, messageName, ns, baseClassName, messageTypeEnumValue);
             codeProvider.GenerateCodeFromCompileUnit(unit, options, filePath);
         }
 
+        private static string GetMessageClassName(Message message)
+        {
+            string messageClassName = $"{message.Name}Message";
+            return messageClassName;
+        }
+
+        private static void GenerateEnumFiles(string folderPath, IEnumerable<MessageDefinitions.Data.Enum> enums, CodeGeneratorOptions options, CodeDomProvider codeProvider, string ns)
+        {
+            foreach (MessageDefinitions.Data.Enum enumeration in enums)
+            {
+                GenerateEnumFile(folderPath, enumeration, options, codeProvider, ns);
+            }
+        }
+
+        private static void GenerateEnumFile(string folderPath, MessageDefinitions.Data.Enum enumeration, CodeGeneratorOptions options, CodeDomProvider codeProvider, string ns)
+        {
+            string enumerationName = enumeration.Name;
+            string filename = $"{enumerationName}.cs";
+            String filePath = Path.Combine(folderPath, filename);
+            CodeCompileUnit unit = EnumGeneratorHelper.CreateCodeCompileUnit(enumeration, ns);
+            codeProvider.GenerateCodeFromCompileUnit(unit, options, filePath);
+        }
 
     }
 }
--- CodeGenerator.Core/MessageGeneratorHelper.cs
+++ CodeGenerator.Core/MessageGeneratorHelper.cs
@@ -1,36 +1,175 @@
 ﻿using System;
 using System.CodeDom;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using MavLink4Net.MessageDefinitions;
+using MavLink4Net.MessageDefinitions.Data;
+using MavLink4Net.MessageDefinitions.Mappers;
 
 namespace MavLink4Net.CodeGenerator.Core
 {
     class MessageGeneratorHelper
     {
-        public static CodeCompileUnit CreateCodeCompileUnit(string messageClassName)
+        public static CodeCompileUnit CreateCodeCompileUnit(MessageDefinitions.Data.Message message, string className, string ns, string baseClassName, string messageIdValue)
         {
             // Generate the container unit
             CodeCompileUnit codeCompileUnit = new CodeCompileUnit();
 
+            CodeNamespace globalNamespace = new CodeNamespace(String.Empty);
+            codeCompileUnit.Namespaces.Add(globalNamespace);
+
+            // using
+            globalNamespace.Imports.Add(new CodeNamespaceImport("System"));
+            globalNamespace.Imports.Add(new CodeNamespaceImport("System.ComponentModel"));
+
             // Generate the namespace
-            CodeNamespace ns = new CodeNamespace("MavLink4Net.Messages");
-
-            // Add the required imports
-            ns.Imports.Add(new CodeNamespaceImport("System"));
-            ns.Imports.Add(new CodeNamespaceImport("System.Xml.Serialization"));
-
-            codeCompileUnit.Namespaces.Add(ns);
+            CodeNamespace codeNamespace = new CodeNamespace(ns);
+            codeCompileUnit.Namespaces.Add(codeNamespace);
 
             // Declare the class
-            CodeTypeDeclaration classDeclaration = new CodeTypeDeclaration()
+            CodeTypeDeclaration classDeclaration = ToCodeTypeDeclaration(message, className, baseClassName, messageIdValue);
+
+            // Add class to the namespace
+            codeNamespace.Types.Add(classDeclaration);
+
+            return codeCompileUnit;
+        }
+
+        private static CodeTypeDeclaration ToCodeTypeDeclaration(MessageDefinitions.Data.Message message, string className, string baseClassName, string messageIdValue)
+        {
+            CodeTypeDeclaration codeTypeDeclaration = new CodeTypeDeclaration()
             {
-                Name = messageClassName,
+                Name = className,
                 IsClass = true
             };
 
-            // Add class to the namespace
-            ns.Types.Add(classDeclaration);
-            return codeCompileUnit;
+            codeTypeDeclaration.BaseTypes.Add(baseClassName);
+
+            AddConstructor(codeTypeDeclaration, messageIdValue);
+
+            // Add summary comments
+            CodeCommentStatement[] summaryCommentStatements = CodeCommentStatementHelper.GetSummaryCodeCommentStatements(message.Description);
+            codeTypeDeclaration.Comments.AddRange(summaryCommentStatements);
+
+            // Add remarks comments
+            CodeCommentStatement[] remarksCommentStatements = CodeCommentStatementHelper.GetRemarksCodeCommentStatements(message.OriginalName);
+            codeTypeDeclaration.Comments.AddRange(remarksCommentStatements);
+
+            // Add fields
+            foreach (MessageField messageField in message.Fields)
+            {
+                CodeMemberField codeMemberField = ToCodeMemberField(messageField);
+                codeTypeDeclaration.Members.Add(codeMemberField);
+            }
+
+            // Add properties
+            foreach (MessageField messageField in message.Fields)
+            {
+                CodeMemberProperty codeMemberProperty = ToCodeMemberProperty(messageField);
+                codeTypeDeclaration.Members.Add(codeMemberProperty);
+            }
+
+            return codeTypeDeclaration;
+        }
+
+        private static void AddConstructor(CodeTypeDeclaration codeTypeDeclaration, string messageIdValue)
+        {
+            // Declare the constructor
+            CodeConstructor constructor = new CodeConstructor();
+            constructor.Attributes = MemberAttributes.Public | MemberAttributes.Final;
+
+            constructor.BaseConstructorArgs.Add(new CodeArgumentReferenceExpression(messageIdValue));
+
+            codeTypeDeclaration.Members.Add(constructor);
+        }
+
+        private static CodeMemberField ToCodeMemberField(MessageField messageField)
+        {
+            CodeMemberField codeMemberField = new CodeMemberField();
+            codeMemberField.Attributes = MemberAttributes.Private;
+            codeMemberField.Name = GetFieldName(messageField.Name);
+
+            // Type
+            Type type = MessageFieldPrimitiveTypeHelper.GetCSharpType(messageField.Type.PrimitiveType);
+            CodeTypeReference codeTypeReference = GetCodeTypeReference(messageField.Type, type);
+            codeMemberField.Type = codeTypeReference;
+
+            if (messageField.Type.IsArray)
+            {
+                
+                CodeArrayCreateExpression ca1 = new CodeArrayCreateExpression(type, messageField.Type.ArraySize);
+                codeMemberField.InitExpression = ca1;
+            }
+                
+            // Add summary comments
+            CodeCommentStatement[] summaryCommentStatements = CodeCommentStatementHelper.GetSummaryCodeCommentStatements(messageField.Text);
+            codeMemberField.Comments.AddRange(summaryCommentStatements);
+
+            // Add remarks comments
+            CodeCommentStatement[] remarksCommentStatements = CodeCommentStatementHelper.GetRemarksCodeCommentStatements(messageField.OriginalName);
+            codeMemberField.Comments.AddRange(remarksCommentStatements);
+
+
+            return codeMemberField;
+        }
+
+        public static string GetFieldName(string word)
+        {
+            if (String.IsNullOrWhiteSpace(word))
+                return null;
+
+            String firstChar = word.First().ToString().ToLower();
+            String otherChars = word.Substring(1);
+
+            string pascalStyleWord = $"_{firstChar}{otherChars}";
+            return pascalStyleWord;
+        }
+
+        private static CodeTypeReference GetCodeTypeReference(MessageFieldType fieldType, Type type)
+        {
+            if (fieldType.IsEnum)
+            {
+                return new CodeTypeReference(fieldType.Enum);
+            }
+
+            CodeTypeReference codeTypeReference = new CodeTypeReference(type);
+
+            if (fieldType.IsArray)
+                codeTypeReference.ArrayRank = 1;
+
+            return codeTypeReference;
+        }
+
+        private static CodeMemberProperty ToCodeMemberProperty(MessageField messageField)
+        {
+            CodeMemberProperty codeMemberProperty = new CodeMemberProperty();
+            codeMemberProperty.Attributes = MemberAttributes.Public | MemberAttributes.Final;
+            codeMemberProperty.Name = messageField.Name;
+            codeMemberProperty.HasGet = true;
+            
+            // Type
+            MessageFieldType fieldType = messageField.Type;
+            CodeTypeReference codeTypeReference = GetCodeTypeReference(fieldType, MessageFieldPrimitiveTypeHelper.GetCSharpType(fieldType.PrimitiveType));
+            codeMemberProperty.Type = codeTypeReference;
+
+            string fieldName = GetFieldName(messageField.Name);
+
+            // Getter
+            codeMemberProperty.GetStatements.Add(new CodeMethodReturnStatement(
+                new CodeFieldReferenceExpression(
+                    new CodeThisReferenceExpression(), fieldName)));
+
+            // Setter
+            codeMemberProperty.SetStatements.Add(new CodeAssignStatement(
+                new CodeFieldReferenceExpression(
+                    new CodeThisReferenceExpression(), fieldName), new CodePropertySetValueReferenceExpression()));
+
+            // Add summary comments
+            CodeCommentStatement[] summaryCommentStatements = CodeCommentStatementHelper.GetSummaryCodeCommentStatements(messageField.Text);
+            codeMemberProperty.Comments.AddRange(summaryCommentStatements);
+
+            return codeMemberProperty;
         }
     }
 }
