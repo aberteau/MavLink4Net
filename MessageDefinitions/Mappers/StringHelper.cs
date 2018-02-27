37d5cc94f526f9a39dbb828d120bb4c5cd3ace8e
aberteau <amael.berteau@tech-easy.fr>
2018-02-28 00:36:42 +0100

    Ajout StringHelper dans MessageDefinitions.Mappers

---
+++ MessageDefinitions/Mappers/StringHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MavLink4Net.MessageDefinitions.Mappers
+{
+    class StringHelper
+    {
+        private static string NormalizeCarriageReturn(string rawStr)
+        {
+            if (String.IsNullOrWhiteSpace(rawStr))
+                return null;
+
+            string str = rawStr.Replace("\r\n", "\n").Replace("\r", "\n").Replace("\n", "\r\n");
+            return str;
+        }
+
+        public static string TrimAndNormalizeCarriageReturn(string rawDescription)
+        {
+            if (String.IsNullOrWhiteSpace(rawDescription))
+                return null;
+
+            string trimStr = rawDescription.Trim();
+            string str = NormalizeCarriageReturn(trimStr);
+            return str;
+        }
+    }
+}
