81ce231fe809756b9c99a119695b35da7eb8cf10
aberteau <amael.berteau@tech-easy.fr>
2018-02-28 00:43:32 +0100

    Adding project MavLink4Net.Messages

---
+++ Messages/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MavLink4Net.Messages
+{
+    public class Message
+    {
+        public MavType MavType { get; }
+
+        protected Message(MavType mavType)
+        {
+            MavType = mavType;
+        }
+    }
+}
