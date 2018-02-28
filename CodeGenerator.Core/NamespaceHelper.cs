28c31f61e7d0409f3356b84ea63ce6b7dc135b5d
aberteau <amael.berteau@tech-easy.fr>
2018-02-28 14:08:54 +0100

    NamespaceHelper added

---
+++ CodeGenerator.Core/NamespaceHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MavLink4Net.CodeGenerator.Core
+{
+    class NamespaceHelper
+    {
+        public static String Combine(String[] s)
+        {
+            string ns = String.Join(".", s);
+            return ns;
+        }
+
+        public static String Combine(String s1, String s2)
+        {
+            string[] strArray = new[] { s1, s2 };
+            string ns = Combine(strArray);
+            return ns;
+        }
+
+        public static String GetNamespace(String s1, String s2)
+        {
+            string ns = Combine(s1, s2);
+            return ns;
+        }
+
+        public static String GetFullname(String s1, String s2)
+        {
+            string ns = Combine(s1, s2);
+            return ns;
+        }
+    }
+}
