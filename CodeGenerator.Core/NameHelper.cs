87bd15662f29dbb35757c9717ab6348563e137ab
aberteau <amael.berteau@tech-easy.fr>
2018-02-28 23:41:53 +0100

    NameHelper added to CodeGenerator.Core

---
+++ CodeGenerator.Core/NameHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MavLink4Net.MessageDefinitions.Data;
+
+namespace MavLink4Net.CodeGenerator.Core
+{
+    class NameHelper
+    {
+        public static string GetMessageClassName(Message message)
+        {
+            string messageClassName = $"{message.Name}Message";
+            return messageClassName;
+        }
+
+        public static string GetSerializerClassName(Message message)
+        {
+            string messageName = GetMessageClassName(message);
+            string serializerClassName = $"{messageName}Serializer";
+            return serializerClassName;
+        }
+    }
+}
