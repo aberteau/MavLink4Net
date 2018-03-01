3783a56f895c34021ce1bc3aca98e630ab3c022c
aberteau <amael.berteau@tech-easy.fr>
2018-03-01 10:12:50 +0100

    Adding Messages.Serialization project

---
+++ Messages.Serialization/IMessageSerializer.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace MavLink4Net.Messages.Serialization
+{
+    public interface IMessageSerializer
+    {
+        void Serialize(BinaryWriter writer, Message message);
+        Message Deserialize(BinaryReader reader);
+    }
+}
---
+++ Messages.Serialization/MessageSerializationHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MavLink4Net.Messages.Serialization
+{
+    class MessageSerializationHelper
+    {
+        private static IMessageSerializer CreateSerializer(MavMessageType mavType)
+        {
+            IMessageSerializer serializer = MessageSerializerFactory.CreateSerializer(mavType);
+            return serializer;
+        }
+
+        public static void Serialize(BinaryWriter writer, Message message)
+        {
+            IMessageSerializer serializer = CreateSerializer(message.MavType);
+            serializer?.Serialize(writer, message);
+        }
+
+        public static Message Deserialize(BinaryReader reader, MavMessageType mavType)
+        {
+            IMessageSerializer serializer = CreateSerializer(mavType);
+            Message message = serializer?.Deserialize(reader);
+            return message;
+        }
+    }
+}
