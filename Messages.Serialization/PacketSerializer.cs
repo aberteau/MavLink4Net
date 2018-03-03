7138fc54fb9f8aa028964006d587f549dab01577
aberteau <amael.berteau@tech-easy.fr>
2018-03-03 15:36:38 +0100

    Adding Packet and PacketSerializer

---
+++ Messages.Serialization/PacketSerializer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MavLink4Net.Messages.Serialization
+{
+    class PacketSerializer
+    {
+        public static Packet Deserialize(BinaryReader reader)
+        {
+            Packet packet = new Packet()
+            {
+                PayLoadLength = reader.ReadByte(),
+                PacketSequenceNumber = reader.ReadByte(),
+                SystemId = reader.ReadByte(),
+                ComponentId = reader.ReadByte(),
+                MessageId = reader.ReadByte(),
+            };
+
+            // Read the payload instead of deserializing so we can validate CRC.
+            packet.Payload = reader.ReadBytes(packet.PayLoadLength);
+            packet.Checksum1 = reader.ReadByte();
+            packet.Checksum2 = reader.ReadByte();
+
+            if (packet.IsValidCrc())
+            {
+                packet.DeserializeMessage();
+            }
+
+            return packet;
+        }
+
+        private void DeserializeMessage()
+        {
+            UasMessage result = UasSummary.CreateFromId(MessageId);
+
+            if (result == null) return;  // Unknown type
+
+            using (MemoryStream ms = new MemoryStream(Payload))
+            {
+                using (BinaryReader br = GetBinaryReader(ms))
+                {
+                    result.DeserializeBody(br);
+                }
+            }
+
+            Message = result;
+            IsValid = true;
+        }
+
+        public static BinaryReader GetBinaryReader(Stream s)
+        {
+            return new BinaryReader(s, Encoding.ASCII);
+        }
+
+
+        // __ Serialization ___________________________________________________
+
+
+        public static MavLinkPacket GetPacketForMessage(
+            UasMessage msg, byte systemId, byte componentId, byte sequenceNumber)
+        {
+            MavLinkPacket result = new MavLinkPacket()
+            {
+                SystemId = systemId,
+                ComponentId = componentId,
+                PacketSequenceNumber = sequenceNumber,
+                MessageId = msg.MessageId,
+                Message = msg
+            };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (BinaryWriter bw = new BinaryWriter(ms))
+                {
+                    msg.SerializeBody(bw);
+                }
+
+                result.Payload = ms.ToArray();
+                result.PayLoadLength = (byte)result.Payload.Length;
+                result.UpdateCrc();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Messages/Packet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MavLink4Net.Messages
+{
+    public class Packet
+    {
+        public const int PacketOverheadNumBytes = 7;
+
+        public byte PayLoadLength;
+        public byte PacketSequenceNumber;
+        public byte SystemId;
+        public byte ComponentId;
+        public byte MessageId;
+        public byte[] Payload;
+        public byte Checksum1;
+        public byte Checksum2;
+    }
+}
